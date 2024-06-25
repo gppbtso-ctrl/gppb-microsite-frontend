5f3a098425ac601f576c156b0fc7751922b454d5
justin062489 <88645591+justin062489@users.noreply.github.com>
2024-06-25 11:56:29 +0800

    remodeled cards committee

--- src/components/committee/main.js
+++ src/components/committee/main.js
@@ -33,7 +33,6 @@ const CommitteeMain = () => {
     getCommittees
   );
 
-  console.log(decodedToken, "from main committee");
   return (
     <div className=" relative w-full h-full flex flex-col justify-center items-center z-10">
       <div>
@@ -43,73 +42,35 @@ const CommitteeMain = () => {
         </Button>:null : null}
         
       </div> */}
-        <div className="relative grid grid-cols-1 lg:grid-cols-2  max-w-[30rem] lg:max-w-[70rem] gap-8 z-40 mt-3">
+        <div className="relative grid grid-cols-1 lg:grid-cols-2  max-w-[30rem] lg:max-w-[70rem] gap-10 z-40 mt-3">
           {isLoading ? (
             <CardsLoading />
           ) : (
             data?.map((committee, index) => (
-              <Card
-                key={index}
-                className="mt-1 w-[28rem] md:w-[28rem] lg:w-[30rem] rounded-none border border-gray-300 "
-              >
+              <Card className="relative shadow-lg grid h-full w-full max-w-[28rem] items-end justify-center overflow-hidden text-center">
                 <CardHeader
-                  color="blue-gray"
-                  className=" h-[17rem]   max-w-full mx-0 rounded-none mt-0 shadow-md "
+                  floated={false}
+                  shadow={false}
+                  color="transparent"
+                  className="absolute inset-0 m-0 h-full w-full rounded-none"
                 >
-                  <img
-                    src={committee.photo_id}
-                    alt="card-image"
-                    className=" object-fill h-full w-full "
-                  />
+                  <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/60 via-black/50" />
+                  <img src={committee.photo_id ?? "/default-com.webp"} />{" "}
                 </CardHeader>
-                <CardBody className="pt-4 flex flex-col gap-1">
-                  <div className="flex  items-center gap-2">
-                    <Link href={`/committee/${committee.id}`}>
-                      <Typography
-                        variant="h6"
-                        color="blue-gray"
-                        className=" text-lg font-monserrat hover:text-blue-400 transition-all duration-300 cursor-pointer"
-                      >
-                        {committee.title}
-                      </Typography>
-                    </Link>
-                    {loaded ? (
-                      decodedToken?.committee_list?.includes(committee.id) ? (
-                        <Typography className="text-sm text-yellow-400">
-                          <FontAwesomeIcon icon={faStar} />
-                        </Typography>
-                      ) : null
-                    ) : null}
-                    {/* <Typography
-                      variant="lead"
-                      color="blue-gray"
-                      className=" text-md font-monserrat"
-                    >
-                      Follow
-                    </Typography> */}
-                  </div>
-                  <div className="mb-2 flex flex-row gap-3">
-                    {/* <Typography className="text-sm">
-                      <FontAwesomeIcon icon={faEye} className=" font-thin " />
-                      <span className=" font-montserrat"> 0 </span>
-                    </Typography> */}
-
-                    <Typography className="text-sm ">
+                <CardBody className="flex flex-col h-[15rem] justify-end z-10  p-6">
+                  <Typography
+                    variant="h3"
+                    color="white"
+                    className=" text-blue-50 font-montserrat font-medium leading-[1.5]"
+                  >
+                    {committee.title}
+                  </Typography>
+                  <Typography className=" text-white self-start">
+                    <span className="flex gap-1 items-center">
                       <FontAwesomeIcon icon={faComment} />
-                      <span className="font-montserrat">
-                        {" "}
-                        {committee.topic_count}{" "}
-                      </span>
-                    </Typography>
-                  </div>
-                  <div className="min-h-10">
-                    <Typography
-                      variant="lead"
-                      className="text-sm font-montserrat h-fit break-words"
-                    >
-                      {committee.description}
-                    </Typography>
-                  </div>
+                      {committee.topic_count}
+                    </span>
+                  </Typography>
                 </CardBody>
               </Card>
             ))
