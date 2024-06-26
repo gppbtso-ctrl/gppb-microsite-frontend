425ab83db332dff4c048b697aa288ce256e8cc88
justin062489 <88645591+justin062489@users.noreply.github.com>
2024-06-26 17:06:38 +0800

    changepassword, fix about ngpa images, footer icons

--- next.config.js
+++ next.config.js
@@ -13,8 +13,8 @@ const nextConfig = {
   env: {
     // BASE_URL: "http://172.16.200.122:8000/",
     // BASE_URL: "https://onlineblacklistingportal.gppb.gov.ph",
-    BASE_URL: "http://localhost",
-    // BASE_URL: "https://ngpa.gppb.gov.ph",
+    // BASE_URL: "http://localhost",
+    BASE_URL: "https://ngpa.gppb.gov.ph",
   },
   reactStrictMode: false,
   output: "standalone",
