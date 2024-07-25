c20b1b3ca45baaff6e3519734d14d2de2b9f7ec7
justin062489 <justinreyes0624@gmail.com>
2024-07-26 00:01:31 +0800

    added animations

---
+++ src/components/home/smoothscrolling.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { ReactLenis, useLenis } from 'lenis/react'
+
+function SmoothScrolling({ children }) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
